6c4b72cab9ca322ab0e0cf1a0e25e9be40fb8a6e
Abhishekparmar123 <53890201+Abhishekparmar123@users.noreply.github.com>
2022-08-04 14:29:42 +0530

    capgemini assessment test problem

---
+++ zabonacciSeries.java
@@ -0,0 +1,36 @@
+ class Result{
+    public int zabonacciNumber(int n){
+        int n1 = 1, n2 = 2, n3, i;
+        System.out.print(n1 + " " + n2);
+
+        if(n>2){
+            for (i = 2; i < n; i++){
+                n3 = (( n1 % 10000) * ( n2 % 10000)) % 10000;
+                n1 = n2;
+                n2 = n3;
+
+                System.out.print(" " + n3);
+            }
+        }
+
+        return n2-n1;
+    }
+}
+
+public class zabonacciSeries {
+    public static void main(String[] args){
+        System.out.println("Args value " + args.length);
+        try{
+            int n = Integer.parseInt(args[0]);
+            Result Result = new Result();
+
+            int result = Result.zabonacciNumber(n);
+            System.out.println("\n\nThe Result : " + result);
+
+            throw new Exception("Provide an argument");
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+    }
+}
